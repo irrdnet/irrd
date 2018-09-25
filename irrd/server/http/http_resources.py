819be36e85997e52e94a509600dda030082b81dd
Sasha Romijn <github@mxsasha.eu>
2018-09-25 11:12:32 +0200

    Refs #25 - Added access list for HTTP status server.

--- irrd/server/http/http_resources.py
+++ irrd/server/http/http_resources.py
@@ -1,7 +1,9 @@
 import logging
 
+from IPy import IP
 from twisted.web import server, resource
 
+from irrd.conf import get_setting
 from .request_handlers import DatabaseStatusRequest
 
 logger = logging.getLogger(__name__)
@@ -13,11 +15,31 @@ class DatabaseStatusResource(resource.Resource):
     """
     isLeaf = True
 
-    def render_GET(self, request):  # noqa: N802
+    def render_GET(self, request: server.Request):  # noqa: N802
         """Render the database status in plain text."""
+        if not self.is_client_permitted(request):
+            request.setResponseCode(403)
+            return b'Access denied'
+
         result_txt = DatabaseStatusRequest().generate_status()
-        request.setHeader("Content-Type", "text/plain; charset=utf-8")
+        request.setHeader(b'Content-Type', b'text/plain; charset=utf-8')
         return result_txt.encode('utf-8')
+
+    def is_client_permitted(self, request) -> bool:
+        """
+        Determine whether a client is permitted to access this interface,
+        based on the server.http.access_list setting.
+        """
+        try:
+            client_ip = IP(request.getClientAddress().host)
+        except (ValueError, AttributeError) as e:
+            logger.critical(f'Rejecting request as HTTP client IP could not be read from '
+                            f'{request.getClientAddress()}: {e}')
+            return False
+
+        access_list_name = get_setting('server.http.access_list')
+        access_list = get_setting(f'access_lists.{access_list_name}')
+        return any([client_ip in IP(allowed) for allowed in access_list])
 
 
 root = resource.Resource()
