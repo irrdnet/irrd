c192d10c05ef43ff3c29192b3b751d9934692fe5
Sasha Romijn <github@mxsasha.eu>
2018-10-03 13:07:50 +0200

    Configure appropriate database connection pool size.

--- irrd/storage/__init__.py
+++ irrd/storage/__init__.py
@@ -2,4 +2,5 @@ import sqlalchemy as sa
 
 from irrd.conf import get_setting
 
-engine = sa.create_engine(get_setting('database_url'))
+database_pool_size = (get_setting('server.whois.max_connections') + len(get_setting('sources'))) * 3
+engine = sa.create_engine(get_setting('database_url'), pool_size=database_pool_size, max_overflow=30)
