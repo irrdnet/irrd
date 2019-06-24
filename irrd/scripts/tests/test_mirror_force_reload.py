2ee0d39c25987db64a3a63f946a6eb46c5d946f0
Sasha Romijn <github@mxsasha.eu>
2019-06-24 17:39:38 +0200

    Fix #233 - Add irrd_mirror_force_reload command. (#235)

---
+++ irrd/scripts/tests/test_mirror_force_reload.py
@@ -0,0 +1,18 @@
+from unittest.mock import Mock
+
+from irrd.utils.test_utils import flatten_mock_calls
+from ..mirror_force_reload import set_force_reload
+
+
+def test_set_force_reload(capsys, monkeypatch):
+    mock_dh = Mock()
+    monkeypatch.setattr('irrd.scripts.mirror_force_reload.DatabaseHandler',
+                        lambda enable_preload_update: mock_dh)
+
+    set_force_reload('TEST')
+    assert flatten_mock_calls(mock_dh) == [
+        ['set_force_reload', ('TEST', ), {}],
+        ['commit', (), {}],
+        ['close', (), {}]
+    ]
+    assert not capsys.readouterr().out
