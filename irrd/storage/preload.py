irrdnet/irrd

commit 73fabe9bfe5fdac5c4fedeb4fe76976c0368b2b8

Fix #251 - Apply correct source filtering for preloaded queries.
All queries that looked for routes originated by a certain AS,
now not correctly filter on the selected sources.
This also fixes an issue where !s-lc would not always be correct.

irrd/storage/preload.py
@@ -1,11 +1,9 @@
-import itertools
-import math
-from collections import defaultdict
-
 import logging
+import math
 import os
 import signal
 import threading
+from collections import defaultdict
 from typing import Optional, List, Set, Dict
 
 from .queries import RPSLDatabaseQuery
@@ -29,8 +27,8 @@ class Preloader:
     store, or conclude an update is already pending anyways.
     """
     def __init__(self):
-        self._origin_route4_store = defaultdict(set)
-        self._origin_route6_store = defaultdict(set)
+        self._origin_route4_store = defaultdict(lambda: defaultdict(set))
+        self._origin_route6_store = defaultdict(lambda: defaultdict(set))
 
         self._reload_lock = threading.Lock()
         self._threads = []
@@ -38,9 +36,10 @@ class Preloader:
         self._store_ready_event = threading.Event()
         self.reload()
 
-    def routes_for_origins(self, origins: List[str], ip_version: Optional[int]=None) -> Set[str]:
+    def routes_for_origins(self, origins: List[str], sources: List[str], ip_version: Optional[int]=None) -> Set[str]:
         """
-        Retrieve all prefixes (in str format) originating from the provided origins.
+        Retrieve all prefixes (in str format) originating from the provided origins,
+        from the given sources.
 
         Prefixes are guaranteed to be unique. ip_version can be set to 4 or 6
         to restrict responses to IPv4 or IPv6 prefixes. Blocks until the first
@@ -53,13 +52,15 @@ class Preloader:
         if ip_version and ip_version not in [4, 6]:
             raise ValueError(f'Invalid IP version: {ip_version}')
 
-        prefix_sets: List[Set[str]] = list()
-        if not ip_version or ip_version == 4:
-            prefix_sets = prefix_sets + [self._origin_route4_store.get(k, set()) for k in origins]
-        if not ip_version or ip_version == 6:
-            prefix_sets = prefix_sets + [self._origin_route6_store.get(k, set()) for k in origins]
+        prefix_sets: Set[str] = set()
+        for origin in origins:
+            for source in sources:
+                if not ip_version or ip_version == 4:
+                    prefix_sets = prefix_sets.union(self._origin_route4_store.get(origin, dict()).get(source, set()))
+                if not ip_version or ip_version == 6:
+                    prefix_sets = prefix_sets.union(self._origin_route6_store.get(origin, dict()).get(source, set()))
 
-        return set(itertools.chain.from_iterable(prefix_sets))
+        return prefix_sets
 
     def reload(self, object_classes_changed: Optional[Set[str]]=None) -> None:
         """
@@ -133,8 +134,8 @@ class PreloadUpdater(threading.Thread):
         self.reload_lock.acquire()
         logger.debug(f'Starting preload store update from thread {self}')
 
-        new_origin_route4_store: Dict[str, Set] = defaultdict(set)
-        new_origin_route6_store: Dict[str, Set] = defaultdict(set)
+        new_origin_route4_store: Dict[str, Dict[str, set]] = defaultdict(lambda: defaultdict(set))
+        new_origin_route6_store: Dict[str, Dict[str, set]] = defaultdict(lambda: defaultdict(set))
 
         if not mock_database_handler:  # pragma: no cover
             from .database_handler import DatabaseHandler
@@ -142,27 +143,26 @@ class PreloadUpdater(threading.Thread):
         else:
             dh = mock_database_handler
 
-        q = RPSLDatabaseQuery(column_names=['ip_version', 'ip_first', 'ip_size', 'asn_first'], enable_ordering=True)
+        q = RPSLDatabaseQuery(column_names=['ip_version', 'ip_first', 'ip_size', 'asn_first', 'source'], enable_ordering=True)
         q = q.object_classes(['route', 'route6'])
 
         for result in dh.execute_query(q):
             prefix = result['ip_first']
+            source = result['source']
             key = 'AS' + str(result['asn_first'])
 
             if result['ip_version'] == 4:
                 length = int(32 - math.log2(result['ip_size']))
-                new_origin_route4_store[key].add(f'{prefix}/{length}')
+                new_origin_route4_store[key][source].add(f'{prefix}/{length}')
             if result['ip_version'] == 6:
                 length = int(128 - math.log2(result['ip_size']))
-                new_origin_route6_store[key].add(f'{prefix}/{length}')
+                new_origin_route6_store[key][source].add(f'{prefix}/{length}')
 
         dh.close()
 
         self.preloader.update_route_store(new_origin_route4_store, new_origin_route6_store)
 
-        logger.info(f'Completed updating preload store from thread {self}, '
-                    f'loaded v4 routes for {len(new_origin_route4_store)} ASes, '
-                    f'v6 routes for {len(new_origin_route6_store)} ASes')
+        logger.info(f'Completed updating preload store from thread {self}')
         self.reload_lock.release()
         self.store_ready_event.set()
 

irrd/server/whois/query_parser.py
@@ -44,7 +44,8 @@ class WhoisQueryParser:
 
     def __init__(self, peer, peer_str: str) -> None:
         self.all_valid_sources = list(get_setting('sources').keys())
-        self.sources: List[str] = []
+        self.sources_default = get_setting('sources_default')
+        self.sources: List[str] = self.sources_default if self.sources_default else self.all_valid_sources
         self.object_classes: List[str] = []
         self.user_agent: Optional[str] = None
         self.multiple_command_mode = False
@@ -198,7 +199,7 @@ class WhoisQueryParser:
         except ValidationError as ve:
             raise WhoisQueryParserException(str(ve))
 
-        prefixes = self.preloader.routes_for_origins([origin_formatted], ip_version=ip_version)
+        prefixes = self.preloader.routes_for_origins([origin_formatted], self.sources, ip_version=ip_version)
         return ' '.join(prefixes)
 
     def handle_irrd_routes_for_as_set(self, set_name: str) -> str:
@@ -218,7 +219,7 @@ class WhoisQueryParser:
 
         self._current_set_root_object_class = 'as-set'
         members = self._recursive_set_resolve({set_name})
-        prefixes = self.preloader.routes_for_origins(members, ip_version=ip_version)
+        prefixes = self.preloader.routes_for_origins(members, self.sources, ip_version=ip_version)
         return ' '.join(prefixes)
 
     def handle_irrd_set_members(self, parameter: str) -> str:
@@ -289,7 +290,8 @@ class WhoisQueryParser:
             try:
                 as_number_formatted, _ = parse_as_number(sub_member)
                 if self._current_set_root_object_class == 'route-set':
-                    set_members.update(self.preloader.routes_for_origins([as_number_formatted]))
+                    set_members.update(self.preloader.routes_for_origins(
+                        [as_number_formatted], self.sources))
                     resolved_as_members.add(sub_member)
                 else:
                     set_members.add(sub_member)
@@ -385,8 +387,7 @@ class WhoisQueryParser:
     def handle_irrd_database_serial_range(self, parameter: str) -> str:
         """!j query - database serial range"""
         if parameter == '-*':
-            default = get_setting('sources_default')
-            sources = default if default else self.all_valid_sources
+            sources = self.sources_default if self.sources_default else self.all_valid_sources
         else:
             sources = [s.upper() for s in parameter.split(',')]
         invalid_sources = [s for s in sources if s not in self.all_valid_sources]
@@ -465,9 +466,7 @@ class WhoisQueryParser:
            !sripe,nttcom limits sources to ripe and nttcom
         """
         if parameter == '-lc':
-            default = get_setting('sources_default')
-            sources_selected = default if default else self.all_valid_sources
-            return ','.join(sources_selected)
+            return ','.join(self.sources)
 
         sources = parameter.upper().split(',')
         if not all([source in self.all_valid_sources for source in sources]):
@@ -570,7 +569,7 @@ class WhoisQueryParser:
                 raise WhoisQueryParserException('One or more selected sources are unavailable.')
             self.sources = sources
         else:
-            self.sources = []
+            self.sources = self.sources_default if self.sources_default else self.all_valid_sources
 
     def handle_ripe_restrict_object_class(self, object_classes) -> None:
         """-T parameter - restrict object classes for this query, comma-seperated"""
@@ -644,7 +643,7 @@ class WhoisQueryParser:
     def _prepare_query(self, column_names=None, ordered_by_sources=True) -> RPSLDatabaseQuery:
         """Prepare an RPSLDatabaseQuery by applying relevant sources/class filters."""
         query = RPSLDatabaseQuery(column_names, ordered_by_sources)
-        if self.sources:
+        if self.sources and self.sources != self.all_valid_sources:
             query.sources(self.sources)
         else:
             default = get_setting('sources_default')

irrd/server/whois/tests/test_query_parser.py
@@ -234,7 +234,7 @@ class TestWhoisQueryParserRIPE:
         assert response.mode == WhoisQueryResponseMode.RIPE
         assert not response.result
         assert flatten_mock_calls(mock_dh) == [['close', (), {}]]
-        assert parser.sources == []
+        assert parser.sources == parser.all_valid_sources
 
     def test_sources_default(self, prepare_parser, config_override):
         mock_dq, mock_dh, mock_preloader, parser = prepare_parser
@@ -501,7 +501,7 @@ class TestWhoisQueryParserIRRD:
         assert response.mode == WhoisQueryResponseMode.IRRD
         assert response.result == '192.0.2.0/25 192.0.2.128/25'
         assert flatten_mock_calls(mock_preloader.routes_for_origins) == [
-            ['', (['AS65547'],), {'ip_version': 4}],
+            ['', (['AS65547'], ['TEST1', 'TEST2']), {'ip_version': 4}],
         ]
 
         mock_preloader.routes_for_origins = Mock(return_value=[])
@@ -517,12 +517,13 @@ class TestWhoisQueryParserIRRD:
 
         mock_preloader.routes_for_origins = Mock(return_value=['2001:db8::1/128', '2001:db8::/32'])
 
+        parser.sources = ['TEST1']
         response = parser.handle_query('!6AS65547')
         assert response.response_type == WhoisQueryResponseType.SUCCESS
         assert response.mode == WhoisQueryResponseMode.IRRD
         assert response.result == '2001:db8::1/128 2001:db8::/32'
         assert flatten_mock_calls(mock_preloader.routes_for_origins) == [
-            ['', (['AS65547'],), {'ip_version': 6}],
+            ['', (['AS65547'], ['TEST1']), {'ip_version': 6}],
         ]
 
         mock_preloader.routes_for_origins = Mock(return_value=[])
@@ -571,7 +572,7 @@ class TestWhoisQueryParserIRRD:
         assert response.mode == WhoisQueryResponseMode.IRRD
         assert response.result == '192.0.2.0/25 192.0.2.128/25'
         assert flatten_mock_calls(mock_preloader.routes_for_origins) == [
-            ['', ({'AS65547', 'AS65548'},), {'ip_version': None}],
+            ['', ({'AS65547', 'AS65548'}, parser.all_valid_sources), {'ip_version': None}],
         ]
         mock_preloader.routes_for_origins.reset_mock()
 
@@ -580,7 +581,7 @@ class TestWhoisQueryParserIRRD:
         assert response.mode == WhoisQueryResponseMode.IRRD
         assert response.result == '192.0.2.0/25 192.0.2.128/25'
         assert flatten_mock_calls(mock_preloader.routes_for_origins) == [
-            ['', ({'AS65547', 'AS65548'},), {'ip_version': 4}],
+            ['', ({'AS65547', 'AS65548'}, parser.sources), {'ip_version': 4}],
         ]
         mock_preloader.routes_for_origins.reset_mock()
 
@@ -589,7 +590,7 @@ class TestWhoisQueryParserIRRD:
         assert response.mode == WhoisQueryResponseMode.IRRD
         assert response.result == '192.0.2.0/25 192.0.2.128/25'
         assert flatten_mock_calls(mock_preloader.routes_for_origins) == [
-            ['', ({'AS65547', 'AS65548'},), {'ip_version': 6}],
+            ['', ({'AS65547', 'AS65548'}, parser.sources), {'ip_version': 6}],
         ]
         mock_preloader.routes_for_origins.reset_mock()
 
@@ -1023,7 +1024,7 @@ class TestWhoisQueryParserIRRD:
         response = parser.handle_query('!s-lc')
         assert response.response_type == WhoisQueryResponseType.SUCCESS
         assert response.mode == WhoisQueryResponseMode.IRRD
-        assert response.result == 'TEST1,TEST2'
+        assert response.result == 'TEST1'
 
         response = parser.handle_query('!sTEST3')
         assert response.response_type == WhoisQueryResponseType.ERROR

irrd/storage/tests/test_preload.py
@@ -34,7 +34,7 @@ class TestPreloader:
         assert len(preloader._threads) == 1
         mock_preload_updater.reset_mock()
 
-        # First call should be ignored.
+        # First call should be ignored, inetnums are not preloaded
         preloader.reload({'inetnum'})
         preloader.reload({'route'})
 
@@ -90,21 +90,30 @@ class TestPreloader:
         preloader._store_ready_event.set()
 
         preloader.update_route_store(
-            {'AS65547': {'192.0.2.0/25'}, 'AS65546': {'192.0.2.128/25'}},
-            {'AS65547': {'2001:db8::/32'}}
+            {
+                'AS65546': {'TEST2': {'192.0.2.0/25'}},
+                'AS65547': {'TEST1': {'192.0.2.128/25', '198.51.100.0/25'}}
+            },
+            {
+                'AS65547': {'TEST2': {'2001:db8::/32'}}
+            },
         )
 
-        assert preloader.routes_for_origins(['AS65545']) == set()
-        assert preloader.routes_for_origins(['AS65546'], 4) == {'192.0.2.128/25'}
-        assert preloader.routes_for_origins(['AS65547'], 4) == {'192.0.2.0/25'}
-        assert preloader.routes_for_origins(['AS65546'], 6) == set()
-        assert preloader.routes_for_origins(['AS65547'], 6) == {'2001:db8::/32'}
-        assert preloader.routes_for_origins(['AS65546']) == {'192.0.2.128/25'}
-        assert preloader.routes_for_origins(['AS65547']) == {'192.0.2.0/25', '2001:db8::/32'}
-        assert preloader.routes_for_origins(['AS65547', 'AS65546'], 4) == {'192.0.2.0/25', '192.0.2.128/25'}
+        sources = ['TEST1', 'TEST2']
+        assert preloader.routes_for_origins(['AS65545'], sources) == set()
+        assert preloader.routes_for_origins(['AS65546'], sources, 4) == {'192.0.2.0/25'}
+        assert preloader.routes_for_origins(['AS65547'], sources, 4) == {'192.0.2.128/25', '198.51.100.0/25'}
+        assert preloader.routes_for_origins(['AS65546'], sources, 6) == set()
+        assert preloader.routes_for_origins(['AS65547'], sources, 6) == {'2001:db8::/32'}
+        assert preloader.routes_for_origins(['AS65546'], sources) == {'192.0.2.0/25'}
+        assert preloader.routes_for_origins(['AS65547'], sources) == {'192.0.2.128/25', '198.51.100.0/25', '2001:db8::/32'}
+        assert preloader.routes_for_origins(['AS65547', 'AS65546'], sources, 4) == {'192.0.2.0/25', '192.0.2.128/25', '198.51.100.0/25'}
+
+        assert preloader.routes_for_origins(['AS65547', 'AS65546'], ['TEST1']) == {'192.0.2.128/25', '198.51.100.0/25'}
+        assert preloader.routes_for_origins(['AS65547', 'AS65546'], ['TEST2']) == {'192.0.2.0/25', '2001:db8::/32'}
 
         with pytest.raises(ValueError) as ve:
-            preloader.routes_for_origins(['AS65547'], 2)
+            preloader.routes_for_origins(['AS65547'], sources, 2)
         assert 'Invalid IP version: 2' in str(ve)
 
 
@@ -124,18 +133,28 @@ class TestPreloadUpdater:
                 'ip_first': '192.0.2.0',
                 'ip_size': 128,
                 'asn_first': 65546,
+                'source': 'TEST1',
             },
             {
                 'ip_version': 4,
                 'ip_first': '192.0.2.128',
                 'ip_size': 128,
                 'asn_first': 65547,
+                'source': 'TEST1',
+            },
+            {
+                'ip_version': 4,
+                'ip_first': '198.51.100.0',
+                'ip_size': 128,
+                'asn_first': 65547,
+                'source': 'TEST1',
             },
             {
                 'ip_version': 6,
                 'ip_first': '2001:db8::',
                 'ip_size': 79228162514264337593543950336,  # /32
                 'asn_first': 65547,
+                'source': 'TEST2',
             },
         ]
         mock_database_handler.execute_query = lambda query: mock_query_result
@@ -148,7 +167,15 @@ class TestPreloadUpdater:
         assert flatten_mock_calls(mock_preload_obj) == [
             [
                 'update_route_store',
-                ({'AS65546': {'192.0.2.0/25'}, 'AS65547': {'192.0.2.128/25'}}, {'AS65547': {'2001:db8::/32'}}),
+                (
+                    {
+                        'AS65546': {'TEST1': {'192.0.2.0/25'}},
+                        'AS65547': {'TEST1': {'192.0.2.128/25', '198.51.100.0/25'}}
+                    },
+                    {
+                        'AS65547': {'TEST2': {'2001:db8::/32'}}
+                    },
+                ),
                 {}
             ]
         ]

commit 1ba7d31dd1647fd58101f0ce351556428592bdac

Fix #251 - Fix performance issue introduced in 73fabe9bfe5fdac5c4fedeb4fe76976c0368b2b8
In some cases, huge amounts of briefly existing dict and set
objects were created, likely causing issues for the GC.

irrd/storage/preload.py
@@ -56,9 +56,15 @@ class Preloader:
         for origin in origins:
             for source in sources:
                 if not ip_version or ip_version == 4:
-                    prefix_sets = prefix_sets.union(self._origin_route4_store.get(origin, dict()).get(source, set()))
+                    try:
+                        prefix_sets.update(self._origin_route4_store[origin][source])
+                    except KeyError:
+                        pass
                 if not ip_version or ip_version == 6:
-                    prefix_sets = prefix_sets.union(self._origin_route6_store.get(origin, dict()).get(source, set()))
+                    try:
+                        prefix_sets.update(self._origin_route6_store[origin][source])
+                    except KeyError:
+                        pass
 
         return prefix_sets
 