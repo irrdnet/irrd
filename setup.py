6289ac71bb1b9d05cc68a8a8de07f45809464677
Sasha Romijn <github@mxsasha.eu>
2019-11-12 14:01:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,19 +23,19 @@ setuptools.setup(
         # This list must be kept in sync with requirements.txt version-wise,
         # but should not include packages used for testing, generating docs
         # or packages.
-        'python-gnupg==0.4.4',
+        'python-gnupg==0.4.5',
         'passlib==1.7.1',
-        'IPy==1.0.0',
-        'dataclasses==0.6',
+        'IPy==1.00',
+        'dataclasses==0.7',
         'ordered-set==3.1.1',
         'dotted==0.1.8',
         'beautifultable==0.8.0',
-        'PyYAML==5.1.1',
-        'psycopg2-binary==2.8.3',
-        'SQLAlchemy==1.3.8',
-        'alembic==1.2.1',
+        'PyYAML==5.1.2',
+        'psycopg2-binary==2.8.4',
+        'SQLAlchemy==1.3.10',
+        'alembic==1.3.0',
         'ujson==1.35',
-        'twisted==19.2.1',
+        'twisted==19.7.0',
     ],
     entry_points={
         'console_scripts': [
