3557977f7cff976929e72d22ed13636a06bfe0ef
Sasha Romijn <github@mxsasha.eu>
2020-12-01 21:40:05 +0100

    Add docs and setup.py for Python 3.9 support

--- setup.py
+++ setup.py
@@ -74,6 +74,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
     ],
 )
