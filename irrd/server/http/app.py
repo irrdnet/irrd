0540fc9a725c9fd12a6eeea6f8caed9a7dde4c58
Sasha Romijn <github@mxsasha.eu>
2020-12-02 11:45:54 +0100

    Separate endpoints and add test for StatusApp

--- irrd/server/http/app.py
+++ irrd/server/http/app.py
@@ -3,16 +3,13 @@ import os
 from ariadne.asgi import GraphQL
 from setproctitle import setproctitle
 from starlette.applications import Starlette
-from starlette.endpoints import HTTPEndpoint
-from starlette.responses import PlainTextResponse
 from starlette.routing import Mount
 
-from irrd.server.access_check import is_client_permitted
 from irrd.server.graphql import ENV_UVICORN_WORKER_CONFIG_PATH
 from irrd.server.graphql.extensions import error_formatter, QueryMetadataExtension
 from irrd.server.graphql.resolvers import init_resolvers, close_resolvers
 from irrd.server.graphql.schema_builder import build_executable_schema
-from irrd.server.http.status_generator import StatusGenerator
+from .endpoints import StatusEndpoint
 
 """
 Starlette app and GraphQL sub-app.
@@ -40,18 +37,8 @@ graphql = GraphQL(
     error_formatter=error_formatter,
 )
 
-
-class StatusApp(HTTPEndpoint):
-    def get(self, request):
-        if not is_client_permitted(request.client.host, 'server.http.status_access_list'):
-            return PlainTextResponse('Access denied', status_code=403)
-
-        response = StatusGenerator().generate_status()
-        return PlainTextResponse(response)
-
-
 routes = [
-    Mount("/v1/status", StatusApp),
+    Mount("/v1/status", StatusEndpoint),
     Mount("/graphql", graphql),
 ]
 
---
+++ irrd/server/http/endpoints.py
@@ -0,0 +1,16 @@
+from starlette.endpoints import HTTPEndpoint
+from starlette.responses import PlainTextResponse
+
+from irrd.server.access_check import is_client_permitted
+from .status_generator import StatusGenerator
+
+
+class StatusEndpoint(HTTPEndpoint):
+    def get(self, request):
+        if not is_client_permitted(request.client.host, 'server.http.status_access_list'):
+            return PlainTextResponse('Access denied', status_code=403)
+
+        response = StatusGenerator().generate_status()
+        return PlainTextResponse(response)
+
+
