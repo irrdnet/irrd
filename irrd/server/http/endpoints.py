c3bfdf9517fe4a13c69ea0bf8b1df0425ff83732
Sasha Romijn <github@mxsasha.eu>
2020-12-03 16:49:35 +0100

    Fix #440 - Add whois HTTP query logging

--- irrd/server/http/endpoints.py
+++ irrd/server/http/endpoints.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from starlette.endpoints import HTTPEndpoint
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse, Response
@@ -6,6 +9,8 @@ from irrd.server.access_check import is_client_permitted
 from .status_generator import StatusGenerator
 from ..whois.query_parser import WhoisQueryParser
 from ..whois.query_response import WhoisQueryResponseType
+
+logger = logging.getLogger(__name__)
 
 
 class StatusEndpoint(HTTPEndpoint):
@@ -19,16 +24,24 @@ class StatusEndpoint(HTTPEndpoint):
 
 class WhoisQueryEndpoint(HTTPEndpoint):
     def get(self, request: Request) -> Response:
+        start_time = time.perf_counter()
         if 'q' not in request.query_params:
             return PlainTextResponse('Missing required query parameter "q"', status_code=400)
+        client_str = request.client.host + ':' + str(request.client.port)
+        query = request.query_params['q']
+
         parser = WhoisQueryParser(
             request.client.host,
-            request.client.host + ':' + str(request.client.port),
+            client_str,
             request.app.state.preloader,
             request.app.state.database_handler
         )
-        response = parser.handle_query(request.query_params['q'])
+        response = parser.handle_query(query)
         response.clean_response()
+
+        elapsed = time.perf_counter() - start_time
+        logger.info(f'{client_str}: sent answer to HTTP query, elapsed {elapsed:.9f}s, '
+                    f'{len(response.result)} chars: {query}')
 
         if response.response_type == WhoisQueryResponseType.ERROR_INTERNAL:
             return PlainTextResponse(response.result, status_code=500)
