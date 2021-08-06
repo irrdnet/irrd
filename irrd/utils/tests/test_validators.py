2106f09e2ab7c3a929cb13d1d8d476d74d228e81
Sasha Romijn <github@mxsasha.eu>
2021-08-06 21:57:52 +0200

    Fix #535 - Fix validator to allow AS4294967295

--- irrd/utils/tests/test_validators.py
+++ irrd/utils/tests/test_validators.py
@@ -8,7 +8,7 @@ from ..validators import parse_as_number, ValidationError, RPSLChangeSubmission
 def test_validate_as_number():
     assert parse_as_number('AS00') == ('AS0', 0)
     assert parse_as_number('AS012345') == ('AS12345', 12345)
-    assert parse_as_number('as4294967290') == ('AS4294967290', 4294967290)
+    assert parse_as_number('as4294967295') == ('AS4294967295', 4294967295)
     assert parse_as_number('012345', permit_plain=True) == ('AS12345', 12345)
     assert parse_as_number(12345, permit_plain=True) == ('AS12345', 12345)
 
@@ -21,7 +21,7 @@ def test_validate_as_number():
     assert 'number part is not numeric' in str(ve.value)
 
     with raises(ValidationError) as ve:
-        parse_as_number('AS429496729999')
+        parse_as_number('AS4294967296')
     assert 'valid range is' in str(ve.value)
 
 
