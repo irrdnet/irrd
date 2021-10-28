b06f412844e792d6237153cf3d9c63b9047785cf
Sasha Romijn <github@mxsasha.eu>
2021-10-28 19:38:57 +0200

    Ref #571 - Trim memory after each whois connection

--- irrd/utils/process_support.py
+++ irrd/utils/process_support.py
@@ -1,3 +1,4 @@
+import ctypes
 import logging
 import os
 import signal
@@ -7,6 +8,8 @@ logger = logging.getLogger(__name__)
 
 
 # Covered by integration tests
+
+
 class ExceptionLoggingProcess(Process):  # pragma: no cover
     def run(self) -> None:
         try:
@@ -15,3 +18,11 @@ class ExceptionLoggingProcess(Process):  # pragma: no cover
             logger.critical(f'Essential IRRd subprocess encountered a fatal error, '
                             f'traceback follows, shutting down: {e}', exc_info=e)
             os.kill(os.getppid(), signal.SIGTERM)
+
+
+def memory_trim():  # pragma: no cover
+    # See https://github.com/irrdnet/irrd/issues/571
+    try:
+        ctypes.CDLL(None).malloc_trim(0)
+    except Exception:
+        pass
