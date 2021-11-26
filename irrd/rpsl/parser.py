f10f7e45d02231b6cdfa69570798076450401210
Sasha Romijn <github@mxsasha.eu>
2021-11-26 23:26:56 +0100

    Comment update

--- irrd/rpsl/parser.py
+++ irrd/rpsl/parser.py
@@ -74,7 +74,7 @@ class RPSLObject(metaclass=RPSLObjectMeta):
     rpki_relevant = False
     # Fields whose values are discarded during parsing
     discarded_fields: List[str] = []
-    # Fields that are ignored in validation even 
+    # Fields that are ignored in validation even
     # for authoritative objects (see #587 for example).
     ignored_validation_fields: List[str] = ['last-modified']
 
