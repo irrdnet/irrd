2413f719e095190f710046e021c3c752b29bd170
Sasha Romijn <github@mxsasha.eu>
2023-01-06 12:23:11 +0100

    Update Python version specifiers in setup.py

--- setup.py
+++ setup.py
@@ -81,11 +81,11 @@ setuptools.setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Operating System :: OS Independent',
     ],
 )
