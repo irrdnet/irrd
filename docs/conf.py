ce21983b981f0ee948dfe3db58c4c2978d1e03a9
Sasha Romijn <github@mxsasha.eu>
2023-02-01 21:16:40 +0100

    Update to doc footer

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'IRRd'
-copyright = u'2018-2020, NTT Ltd.'
+copyright = u'2018-2023, NTT Ltd. and Reliably Coded B.V.'
 author = u"IRRd developers"
 
 # The version info for the project you're documenting, acts as replacement
