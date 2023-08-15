dd8cb3d04f0b8f66e9de777ac56bf93ffb67a00c
Sasha Romijn <github@mxsasha.eu>
2023-08-15 22:25:03 +0200

    disable in unit tests

--- irrd/utils/process_support.py
+++ irrd/utils/process_support.py
@@ -59,4 +59,5 @@ def set_traceback_handler():  # pragma: no cover
             code += traceback.format_list(traceback.extract_stack(stack))
         logger.info("".join(code))
 
-    signal.signal(signal.SIGUSR1, sigusr1_handler)
+    if not getattr(sys, '_called_from_test', False):
+        signal.signal(signal.SIGUSR1, sigusr1_handler)
