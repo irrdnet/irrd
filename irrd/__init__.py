579f0b301a1984e3777b224e41dd629ae3c6c1b5
Sasha Romijn <github@mxsasha.eu>
2026-01-30 14:30:39 +0100

    Bump version to 4.5.0 🎉

--- irrd/__init__.py
+++ irrd/__init__.py
@@ -1,5 +1,5 @@
 # Note that version is also in pyproject.toml
-__version__ = "4.5-dev"
+__version__ = "4.5.0"
 ENV_MAIN_PROCESS_PID = "IRRD_MAIN_PROCESS_PID"
 ENV_MAIN_STARTUP_TIME = "IRRD_MAIN_STARTUP_TIME"
 META_KEY_HTTP_CLIENT_IP = "HTTP-Client-IP"
